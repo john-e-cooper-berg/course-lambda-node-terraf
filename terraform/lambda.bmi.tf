6a9aab260b2b7296d2f437aeb42c9124cffb93d5
John <john.e.cooper.berg@gmail.com>
2020-01-10 20:26:49 +1100

    terraform - after fmt

--- terraform/lambda.bmi.tf
+++ terraform/lambda.bmi.tf
@@ -3,19 +3,19 @@ provider "aws" {
 }
 
 resource "aws_lambda_function" "lambda" {
-  function_name = "${var.stage}-bmi"
-  handler = "src/index.performBmiCalculation"
-  memory_size = "256"
-  timeout = 10
+  function_name                  = "${var.stage}-bmi"
+  handler                        = "src/index.performBmiCalculation"
+  memory_size                    = "256"
+  timeout                        = 10
   reserved_concurrent_executions = "-1"
-  s3_bucket = "lambdas-bmicalc"
-  s3_key = var.lambda_version
-  role = aws_iam_role.bmi_lambda_iam_role.arn
-  runtime = "nodejs12.x"
+  s3_bucket                      = "lambdas-bmicalc"
+  s3_key                         = var.lambda_version
+  role                           = aws_iam_role.bmi_lambda_iam_role.arn
+  runtime                        = "nodejs12.x"
 }
 
 resource "aws_iam_role" "bmi_lambda_iam_role" {
-  name = "bmi_role_terr"
+  name               = "bmi_role_terr"
   assume_role_policy = <<EOF
 {
  "Version": "2012-10-17",
